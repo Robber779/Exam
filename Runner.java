5087f1a860dafce5bc6c7f4a157f89125291ab41
Robber779 <2jrdavis779@gmail.com>
2025-04-27 22:11:46 -0400

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -11,15 +11,40 @@ public class Runner {
             String input;     // To hold input
             int questions;    // Number of questions
             int missed;       // Number of questions missed
-
-            // Get the number of questions on the exam
-            input = JOptionPane.showInputDialog("How many questions are on the exam?");
-            questions = Integer.parseInt(input);
-
+            
+            while(true){
+               try{
+                  input = JOptionPane.showInputDialog("How many questions are on the exam?");
+                  questions = Integer.parseInt(input);
+   
+                  if (questions <= 0) {
+                     JOptionPane.showMessageDialog(null, "Number of questions must be greater than zero.");
+                     continue;
+                  }
+   
+                  break;//without this, it cant move on to the code after this. Used ai to help figure this part out because I couldn't find a way to stop the error.
+               } catch(NumberFormatException e){
+                  JOptionPane.showMessageDialog(null, "Please enter a valid number.");
+               }
+            
+            }     
+                   
             // Get the number of questions the student missed
-            input = JOptionPane.showInputDialog("How many questions did the student miss?");
-            missed = Integer.parseInt(input);
+            while(true){
+               try{
+                  input = JOptionPane.showInputDialog("How many questions did the student miss?");
+                  missed = Integer.parseInt(input);
 
+                  if(missed<0 || missed>questions){
+                     JOptionPane.showMessageDialog(null, "Missed questions must be between 0 and " + questions + ".");
+                     continue;
+                  }
+
+                  break;//same goes for this one
+               } catch(NumberFormatException e){
+                  JOptionPane.showMessageDialog(null, "Please enter a valid number.");
+               }
+            }
             // Create an Exam object
             Exam exam = new Exam(questions, missed);
 
@@ -28,7 +53,7 @@ public class Runner {
             message += " points.\nThe exam score is " + exam.getScore();
             message += "\nThe exam grade is " + exam.getGrade();
             JOptionPane.showMessageDialog(null, message);
-
+                        
             System.exit(0);
         }
     }
